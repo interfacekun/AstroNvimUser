a37d47e1acdcb7c16be10a31c99efec9756a0f33
interfacekun <qq627795061@outlook.com>
2023-12-25 15:32:26 +0800

    Trans.vim

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,19 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "JuanZoran/Trans.nvim",
+    build = function () require'Trans'.install() end,
+    keys = {
+    -- 可以换成其他你想映射的键
+      { 'fy', mode = { 'n', 'x' }, '<Cmd>Translate<CR>', desc = ' Translate' },
+      { 'fs', mode = { 'n', 'x' }, '<Cmd>TransPlay<CR>', desc = ' Auto Play' },
+      -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
+      { 'fi', '<Cmd>TranslateInput<CR>', desc = ' Translate From Input' },
+      },
+      dependencies = { 'kkharji/sqlite.lua', },
+      opts = {
+        -- your configuration there
+      }
+  },
 }
